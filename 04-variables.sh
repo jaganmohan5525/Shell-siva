ab5b8f7ecc48aa317e431be6494aef8169c88c7f
jaganmohan5525 <jaganmohan5525@gmail.com>
2024-04-25 15:10:34 +0530

    added shellscript

---
+++ 04-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Declaring a variable through command line prompt
+
+PERSON1=$1
+PERSON2=$2
+
+#Referencing variables
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you..?"
+echo "$PERSON1:: I am fine too. How is your work going on..?"
+echo "$PERSON2:: Not bad. I am thinking to learn and upgradeto AWS/AZURE/GCP/DevOps"
+
---
+++ 06-array.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MOVIES=("Premalu" "yatra2" "RajadhanFiles")
+
+# size of the above array is 3.
+
+# index are 0,1,2
+
+#List always starts with 0.
+
+echo "First movie is: ${MOVIES[0]}"
+echo "Second movie is: ${MOVIES[1]}"
+echo "Third movie is: ${MOVIES[2]}"
+
