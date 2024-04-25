01c676b2802f6af413bbb2d899a744064f458ad9
jaganmohan5525 <jaganmohan5525@gmail.com>
2024-04-25 15:19:29 +0530

    added shellscript

--- 08-special-datatypes.sh
+++ 08-special-datatypes.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+echo "All variables: $@"
+echo "Number of variables passed: $#"
+echo "Script Name: $0"
+
+echo "current working directory is: $PWD"
+echo "Home directory of current user is: $HOME"
+echo "Which user is running the script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "Process ID of the current shell script: $$"
+
+
+sleep 60 &
+echo "Process ID of Last background command: $!"
+
+
